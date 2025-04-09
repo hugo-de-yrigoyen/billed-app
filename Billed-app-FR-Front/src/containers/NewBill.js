379e2ec7c74a471f2d8ebdddddb8e357a289543f
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2025-04-09 12:53:04 +0200

    prevent user from uploading files other than .jpg, .jpeg or .png

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -16,15 +16,30 @@ export default class NewBill {
     new Logout({ document, localStorage, onNavigate })
   }
   handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
-
+    e.preventDefault();
+  
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    const file = fileInput.files[0];
+  
+    if (!file) return;
+  
+    const validExtensions = ['jpg', 'jpeg', 'png'];
+    const fileExtension = file.name.split('.').pop().toLowerCase();
+  
+    if (!validExtensions.includes(fileExtension)) {
+      alert('Format de fichier non valide. Veuillez sélectionner une image au format .jpg, .jpeg ou .png');
+      fileInput.value = '';
+      return;
+    }
+  
+    const filePath = e.target.value.split(/\\/g);
+    const fileName = filePath[filePath.length - 1];
+    const formData = new FormData();
+    const email = JSON.parse(localStorage.getItem("user")).email;
+  
+    formData.append('file', file);
+    formData.append('email', email);
+  
     this.store
       .bills()
       .create({
@@ -33,12 +48,13 @@ export default class NewBill {
           noContentType: true
         }
       })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+      .then(({ fileUrl, key }) => {
+        console.log(fileUrl);
+        this.billId = key;
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+      })
+      .catch(error => console.error(error));
   }
   handleSubmit = e => {
     e.preventDefault()
